d204f2e7795de52bc6861455f9c11204682b0622
Maziar Shahsavanpour <maziar.sh110@rireup.net>
2020-02-10 17:18:19 +0330

    modify ansible host

--- ansible_NetData/install_netdat.sh
+++ ansible_NetData/install_netdat.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 #You should have set password on docker containers
+#API container and LoadBalancer container ports should be -
+# - the same as what is set in dockerfile and docker compose
 docker_name=$(docker ps --format "{{.Names}}")
 echo $docker_name
 for container in $docker_name
 do 
-# If you want not to install netdata on client, you can add an IF condition
 res=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container)
-echo $res >> ./ansible/host
-ssh-copy-id -f root@$res
+
+if [[ $container != *"client_1"* ]]
+then
+if [[ $container == *"api_1"* ]]
+then 
+port=201
+fi
+if [[ $container == *"loadbalancer_1"* ]]
+then 
+port=203
+fi
+echo $res >> ./host
+echo $container
+echo $res:$port
+
+    ssh-copy-id -f root@$res -p $port
+fi
+
 done
